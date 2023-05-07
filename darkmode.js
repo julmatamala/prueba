d92f11f2f2b6c36bc3ca697a15d21388a434cb00
Rysu <di.guiterrez@duocuc.cl>
2023-05-07 03:44:15 -0400

    Terminado evento modo oscuro
    modo oscuro implementado

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-    $("#darkmode").on(click,function(){
-	    $("#:root").css("--bgcolor3", "black");
-    });
+//global darkmode
+$(document).ready(function() {
+	$('#cambiarFondo').click(function() {
+		$('body').toggleClass('modoOscuro');
+		$('#aboutush1').toggleClass('modoOscuro');
+		$('#registro').toggleClass('modoOscuro');
+		$('#bodyCarrito').toggleClass('modoOscuro');
+	
+	});
 });
+
+
+
+
+
+
+//index
+document.getElementById("petsco").addEventListener("click", function(event) {
+	event.preventDefault();
+	alert("Ya estás en la pagina principal");
+  });
+
+ 
+ 
+  //about us
+  
