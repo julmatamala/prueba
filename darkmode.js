7366279c80e5e971c7f6b9581d0d438af46da1b5
Rysu <di.guiterrez@duocuc.cl>
2023-05-08 03:02:44 -0400

    más eventos
    más eventos y reajustes en los scripts del carrito y del register

--- darkmode.js
+++ darkmode.js
@@ -7,20 +7,39 @@ $(document).ready(function() {
 		$('#bodyCarrito').toggleClass('modoOscuro');
 	
 	});
-});
 
+	$('#petsco').click(function(event){
+		event.preventDefault();
+		alert("Ya estás en la pagina principal");
+	})
 
-
-
-
-
-//index
-document.getElementById("petsco").addEventListener("click", function(event) {
-	event.preventDefault();
-	alert("Ya estás en la pagina principal");
+	// Obtener el campo de entrada y los elementos de contador
+	var input = $('#inputrut');
+	console.log(input);
+	var count = $('#char-count');
+	
+	// Configurar el límite de caracteres
+	var maxChars = 10; // Por ejemplo, 280 caracteres como en Twitter
+	count.text("0/" + maxChars); // Mostrar el contador inicializado
+	
+	// Escuchar el evento "input" en el campo de entrada
+	input.on('input', function() {
+	  // Obtener la longitud actual del texto en el campo de entrada
+	  var len = input.val().length;
+	  
+	  // Actualizar los contadores
+	  count.text(len + "/" + maxChars);
+	  
+	  // Cambiar el color del contador a rojo si se ha alcanzado o superado el límite de caracteres
+	  if (len >= maxChars) {
+		count.css('color', 'red');
+	  } else {
+		count.css('color', '');
+	  }
+	});
   });
-
- 
- 
-  //about us
+  
+  
+  
+  
   
---
+++ register.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    const btnCerrarModal = document.querySelector("#btn-cerrar-modal");
+    const modal = document.querySelector("#modal");
+    
+    btnCerrarModal.addEventListener("click", () => { modal.close(); })
+    
+})
