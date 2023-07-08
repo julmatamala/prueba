ca178a1130a26296f9daca8a17a6403e2fab5902
julio matamala <jul.matamala@duocuc.cl>
2023-07-08 18:45:49 -0400

    rollback pre base de datos

--- petscop/ventas/context_processor.py
+++
@@ -1,9 +0,0 @@
-def total_carrito(request):
-    total = 0
-    if request.user.is_authenticated:
-        if "carrito" in request.session.keys():
-            for key, value in request.session["carrito"].items():
-                # Verificar si la clave "acumulado" existe en el elemento del carrito
-                if "acumulado" in value:
-                    total += int(value["acumulado"])
-    return {"total_carrito": total}
